78fc312ea2c40e66da0e0831b30f6effc89bd501
PappNiki20 <papp.nikoletta001@gmail.com>
2023-09-18 11:24:26 +0200

    pakli kartyatrukk nincs befejezve

--- src/kartyatrukkkonzolon/Pakli.java
+++ src/kartyatrukkkonzolon/Pakli.java
@@ -1,23 +1,10 @@
+
 package kartyatrukkkonzolon;
 
-import java.util.Scanner;
-
-public class KartyaTrukkKonzolon {
-
-   private static String[] pakli = new String[22];
-    private static final Scanner sc = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        feltolt();
-        for (int i = 0; i < 3; i++) {
-           // kirak();
-            int oszlop = melyik();
-            kever(oszlop);
-        }
-        ezVolt();
-    }
-
-    private static void feltolt() {
+public class Pakli {
+   String[] pakli = new String[22];
+  
+    public Pakli() {
         String[] szinek = {"P", "T", "Z", "M"};
         String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
         int i = 1;
@@ -26,31 +13,21 @@ public class KartyaTrukkKonzolon {
                 pakli[i++] = szin + "_" + ertekek[e];
             }
         }
-
+     
     }
-    
-   /* private static void kirak() {
+
+    private void feltolt() {
+        
+    }
+   private void kirak() {
         for (int i = 1; i < pakli.length; i++) {
             System.out.printf("%-8s", pakli[i]);
             if (i % 3 == 0) {
                 System.out.println("");
             }
         }
-    }*/
-
-    private static int melyik() {
-        boolean jo;
-        int oszlop;
-        do {
-            System.out.print("melyik oszlop (1-3): ");
-            oszlop = sc.nextInt();
-            jo = oszlop >= 1 && oszlop <= 3;
-        } while (!jo);
-        return oszlop;
     }
-
-    private static void kever(int oszlop) {
-        // mindig középre a választott
+   private void kever(int oszlop) {
         String[] ujPakli = new String[22];
         switch (oszlop) {
             case 1:
@@ -77,9 +54,7 @@ public class KartyaTrukkKonzolon {
         }
         pakli = ujPakli;
     }
-
-    private static void ezVolt() {
-      //  System.out.println("A választott lap: " + pakli[11]);
+   private void eVolt() {
+          System.out.println("A választott lap: " + pakli[11]);
     }
-
 }
